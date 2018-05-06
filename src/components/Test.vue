218a890ad71e004562c0a39fa57edf5e2e53b25f
Riadhi Aditya <ridzeal@gmail.com>
2018-05-06 17:55:49 +0700

    Add Test layout and v-for implementation example

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class=test>
+    <h1>{{label}}</h1>
+    <ul>
+      <li :key="rider.id" v-for="rider in riders">
+        {{ rider.name }} - {{rider.class}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      label: "Rider List",
+      riders: [
+        {id:1, name: "Kuuga", class: "SS"},
+        {id:2, name: "Decade", class: "S"},
+        {id:3, name: "Hibiki", class: "B"}
+      ]
+    }
+  }
+}
+</script>
