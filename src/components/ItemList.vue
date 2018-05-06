c9de44352e86d22708942545d64c66b4dc3d02ab
Riadhi Aditya <ridzeal@gmail.com>
2018-05-06 21:17:37 +0700

    Separate sub component example

---
+++ src/components/ItemList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="item-list">
+    <li :key="rider.id" v-for="rider in riders">
+      {{ rider.name }} - {{rider.class}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'item-list',
+  data () {
+    return {
+      riders: [
+        {id:1, name: "Kuuga", class: "SS"},
+        {id:2, name: "Decade", class: "S"},
+        {id:3, name: "Hibiki", class: "B"},
+        {id:4, name: "Kabuto", class: "A"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  border: 1px solid;
+}
+</style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,11 +3,7 @@
     <h1>
       <span v-text="label">Rider List</span>
     </h1>
-    <ul>
-      <li :key="rider.id" v-for="rider in riders">
-        {{ rider.name }} - {{rider.class}}
-      </li>
-    </ul>
+    <item-list></item-list>
     <div>
       <input type="text" v-model="label" />
     </div>
@@ -16,17 +12,13 @@
 </template>
 
 <script>
+import ItemList from './ItemList'
 export default {
   name: 'test',
+  components: { 'item-list': ItemList },
   data () {
     return {
       label: "Rider List",
-      riders: [
-        {id:1, name: "Kuuga", class: "SS"},
-        {id:2, name: "Decade", class: "S"},
-        {id:3, name: "Hibiki", class: "B"},
-        {id:4, name: "Kabuto", class: "A"}
-      ],
       testMe: () => {
         console.log('test');
       }
@@ -41,14 +33,5 @@ button {
   color: white;
   padding: 10px 20px;
   border-radius: 30px;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  border: 1px solid;
 }
 </style>
