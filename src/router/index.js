1e326291b3c2fb076e7917d4283b3ccaa9692bb5
Riadhi Aditya <ridzeal@gmail.com>
2018-05-10 09:16:04 +0700

    Add material design and new main page as example

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import Router from 'vue-router'
+import Main from '@/pages/Main'
 import Login from '@/pages/Login'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
 
 Vue.use(Router)
+Vue.use(VueMaterial)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'main',
+      component: Main
+    }, {
+      path: '/login',
       name: 'login',
       component: Login
     }, {
