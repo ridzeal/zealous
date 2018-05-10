57d19dc30a53dabb21b6184cb7e9d7a477f20027
Riadhi Aditya <ridzeal@gmail.com>
2018-05-10 08:48:23 +0700

    Add Vue Resource with example for REST call

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResouce from 'vue-resource'
 
+Vue.use(VueResouce)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class=login>
     <h1>{{ title }}</h1>
+    <div>Your IP is {{ ip }}</div>
     <div>
       <input type="text" v-model="input.username" placeholder="Username" />
     </div>
@@ -17,6 +18,7 @@ export default {
   data () {
     return {
       title: 'Zealous',
+      ip: '',
       input: {
         username: '',
         password: ''
@@ -25,13 +27,21 @@ export default {
         alert('User: ' + this.input.username + ' / Password: ' + this.input.password)
       }
     }
+  },
+
+  mounted () {
+    this.$http.get('https://httpbin.org/ip').then(result => {
+      this.ip = result.body.origin
+    }, error => {
+      console.error(error)
+    })
   }
 }
 </script>
 
 <style scoped>
 button {
-  background: #A13;
+  background: rgb(44, 30, 243);
   color: white;
   padding: 10px 20px;
   border-radius: 30px;
